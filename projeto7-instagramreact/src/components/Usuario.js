0b545030a8903a547d8ae3deb888ef085fbff325
leonardoyusuke <you@example.comleoyuhirano@gmail.com>
2022-11-20 01:46:41 +0000

    perfil funcionando

--- projeto7-instagramreact/src/components/Usuario.js
+++ projeto7-instagramreact/src/components/Usuario.js
@@ -1,12 +1,27 @@
+import { useState } from "react"
+
 export default function Usuario (){
+  const imagemPadrao = "assets/img/catanacomics.svg"
+  const [nomeUsuario, setNomeUsuario] = useState("")
+  const [fotoPerfil, setFotoPerfil] = useState(imagemPadrao)
+  function inserirUsuario(){
+    const resposta = prompt("Qual seu nome?")
+    setNomeUsuario(resposta)
+    }
+  function inserirFoto(){
+    const respostaFoto = prompt("Url da Foto")
+    setFotoPerfil(respostaFoto)
+  }
+    
+
     return(
         <div class="usuario">
-      <img src="assets/img/catanacomics.svg" />
+      <img onClick={inserirFoto} src={!fotoPerfil ? imagemPadrao : fotoPerfil} />
       <div class="texto">
         <strong>catanacomics</strong>
         <span>
-          Catana
-          <ion-icon name="pencil"></ion-icon>
+        {!nomeUsuario ? "Catana" : `${nomeUsuario}`}
+          <ion-icon onClick={inserirUsuario} name="pencil"></ion-icon>
         </span>
       </div>
     </div>
