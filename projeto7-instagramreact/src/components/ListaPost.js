d83c43ceef7f044454f5284ab6edcd5186c5e38a
leonardoyusuke <you@example.comleoyuhirano@gmail.com>
2022-11-21 15:38:10 +0000

    so falta like clicando na foto

---
+++ projeto7-instagramreact/src/components/ListaPost.js
@@ -0,0 +1,71 @@
+import Post from "./Post"
+import { useState } from "react";
+export default function ListaPost(props){
+    const [nomeIconeSalvar, setNomeIconeSalvar] = useState("bookmark-outline");
+    
+    function salvarPost(){
+        if (nomeIconeSalvar == "bookmark-outline") {
+            setNomeIconeSalvar("bookmark")
+        } else {
+            setNomeIconeSalvar("bookmark-outline")
+        }
+        }
+    
+    const [iconeFotoCurtida , setIconeFotoCurtida] = useState("heart-outline")
+    const [corCoracao , setCorCoracao] = useState("black")
+    let [novoNumeroCurtidas, setNovoNumeroCurtidas] = useState (props.numeroCurtidas);
+
+    function curtirPost (){
+        if(iconeFotoCurtida == "heart-outline"){
+            setIconeFotoCurtida("heart")
+            setCorCoracao("red")
+            setNovoNumeroCurtidas(novoNumeroCurtidas + .001 )
+            console.log(typeof(novoNumeroCurtidas))
+        }else{
+            setIconeFotoCurtida("heart-outline");
+            setCorCoracao("black")
+            setNovoNumeroCurtidas(novoNumeroCurtidas - .001)
+    }
+    }
+
+
+    return (
+        <div class="posts">
+        <div class="post">
+          <div class="topo">
+            <div class="usuario">
+              <img src={props.fotoUsuario} />
+              {props.usuario}
+            </div>
+            <div class="acoes">
+              <ion-icon name="ellipsis-horizontal"></ion-icon>
+            </div>
+          </div>
+
+          <div class="conteudo">
+            <img src={props.imagem} />
+          </div>
+
+          <div class="fundo">
+            <div class="acoes">
+              <div>
+                <ion-icon style={{color: corCoracao}} onClick={(() => curtirPost())} name={iconeFotoCurtida}></ion-icon>
+                <ion-icon name="chatbubble-outline"></ion-icon>
+                <ion-icon name="paper-plane-outline"></ion-icon>
+              </div>
+              <div>
+                <ion-icon onClick={(() => salvarPost())} name={nomeIconeSalvar}></ion-icon>
+              </div>
+            </div>
+
+            <div class="curtidas">
+              <img src={props.imgCurtidas} />
+              <div class="texto">
+                Curtido por <strong>{props.usuarioCurtidas}</strong> e <strong>outras {novoNumeroCurtidas } pessoas</strong>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        )
+}
--- projeto7-instagramreact/src/components/Post.js
+++ projeto7-instagramreact/src/components/Post.js
@@ -1,7 +1,8 @@
+import ListaPost from "./ListaPost"
 export default function Post(){
     const itensPost = [
-        {usuario:"meowed" , fotoUsuario:"assets/img/meowed.svg" , imagem:"assets/img/gato-telefone.svg" , imgCurtidas:"assets/img/respondeai.svg" , usuarioCurtidas:"respondeai" , numeroCurtidas:"101.523"},
-        {usuario:"barked" , fotoUsuario:"assets/img/barked.svg" , imagem:"assets/img/dog.svg" , imgCurtidas:"assets/img/adorable_animals.svg" , usuarioCurtidas:"adorable_animals" , numeroCurtidas:"99.159"}
+        {usuario:"meowed" , fotoUsuario:"assets/img/meowed.svg" , imagem:"assets/img/gato-telefone.svg" , imgCurtidas:"assets/img/respondeai.svg" , usuarioCurtidas:"respondeai" , numeroCurtidas:101.523},
+        {usuario:"barked" , fotoUsuario:"assets/img/barked.svg" , imagem:"assets/img/dog.svg" , imgCurtidas:"assets/img/adorable_animals.svg" , usuarioCurtidas:"adorable_animals" , numeroCurtidas:99.158}
     ]
 
     return (
@@ -9,45 +10,4 @@ export default function Post(){
          {itensPost.map((s) => <ListaPost usuario={s.usuario} imagem={s.imagem} fotoUsuario={s.fotoUsuario} imgCurtidas={s.imgCurtidas} numeroCurtidas={s.numeroCurtidas} />)}
         </div>
     )
-}
-function ListaPost(props){
-    return (
-        <div class="posts">
-        <div class="post">
-          <div class="topo">
-            <div class="usuario">
-              <img src={props.fotoUsuario} />
-              {props.usuario}
-            </div>
-            <div class="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
-            </div>
-          </div>
-
-          <div class="conteudo">
-            <img src={props.imagem} />
-          </div>
-
-          <div class="fundo">
-            <div class="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-
-            <div class="curtidas">
-              <img src={props.imgCurtidas} />
-              <div class="texto">
-                Curtido por <strong>{props.usuarioCurtidas}</strong> e <strong>outras {props.numeroCurtidas} pessoas</strong>
-              </div>
-            </div>
-          </div>
-        </div>
-        </div>
-        )
 }
